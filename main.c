5a57e0c46a5b2488bf460078adb6c3139abe289f
khy252 <khy25@sookmyung.ac.kr>
2025-09-28 19:00:14 +0900

    04

--- main.c
+++ main.c
@@ -3,13 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-    int time;
+    int year;
     
-    printf("input seconds :");
-    scanf("%i", &time);
+    printf("input year :");
+    scanf("%i", &year);
     
-    printf("The time is %i : %i\n", time/60, time%60);
-    
+    printf("Is the year %i leap year? %i\n", 
+                        year,
+                        (year%4==0 && year%100!=0)||(year%400==0)
+                        );
+                        
   system("PAUSE");	
   return 0; 
 }
