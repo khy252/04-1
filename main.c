359924b138f19b9d541053402becdddcc371b0ab
khy252 <khy25@sookmyung.ac.kr>
2025-09-28 18:23:57 +0900

    03

--- main.c
+++ main.c
@@ -3,16 +3,12 @@
 
 int main(int argc, char *argv[])
 {
-    int x, y;
+    int time;
     
-    printf("input two integers :");
-    scanf("%i %i", &x, &y);
+    printf("input seconds :");
+    scanf("%i", &time);
     
-    printf("+ result :%i\n", x+y);
-    printf("- result :%i\n", x-y);
-    printf("* result :%i\n", x*y);
-    printf("/ result :%i\n", x/y);
-    printf("%% result :%i\n", x%y);
+    printf("The time is %i : %i\n", time/60, time%60);
     
   system("PAUSE");	
   return 0; 
